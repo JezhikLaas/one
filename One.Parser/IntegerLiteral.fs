2dcb2b9d49085a225f7dfcbef96e118991787a7c
Jezhik <uwe.laas@domla.de>
2019-05-12 10:30:42 +0200

    Completes integer literal parser

--- One.Parser/IntegerLiteral.fs
+++ One.Parser/IntegerLiteral.fs
@@ -1,3 +1,4 @@
+
 module One.Parser.IntegerLiteral
 
     open System
@@ -20,7 +21,7 @@ module One.Parser.IntegerLiteral
             .Repeat(1)
             .Then(fun firstDigit -> Character.Digit
                                         .Repeat(2)
-                                        .Select(fun secondDigit -> firstDigit.[0] + secondDigit.[0].ToString()))
+                                        .Select(fun twoDigits -> firstDigit.[0] + new string(twoDigits)))
     
     let leftMostThreeDigitGroupParser =
         threeDigitParserWithoutLeadingZeroParser.Try()
@@ -33,25 +34,33 @@ module One.Parser.IntegerLiteral
             .Repeat(3)
             .Select(fun digits -> new string(digits))
 
-    let threeDigitFollowUp =
+    let threeDigitFollowUpParser =
         Character
             .EqualTo('_')
             .Then(fun char -> threeDigitGroupParser
                                   .Repeat(1)
-                                  .Select(fun group -> char.ToString() + String.Join(String.Empty, group)))
+                                  .Select(fun group -> String.Join(String.Empty, group)))
            
     let notZeroIntegerParser =
-        leftMostThreeDigitGroupParser
-            .Repeat(1)
-            .Then(fun head -> threeDigitFollowUp
-                                  .Many()
-                                  .Select(fun tail -> head.[0] + String.Join("", tail)))
+        Character
+            .EqualTo('-')
+            .Optional()
+            .Then(fun optionalMinus -> leftMostThreeDigitGroupParser
+                                        .Repeat(1)
+                                        .Then(fun head -> threeDigitFollowUpParser
+                                                            .Many()
+                                                            .Select(fun tail -> match Option.ofNullable optionalMinus with
+                                                                                | Some minus -> minus.ToString() + head.[0] + String.Join("", tail)
+                                                                                | None       -> head.[0] + String.Join("", tail)
+                                                            )
+                                        )
+             )
     
     let zeroIntegerParser =
         Character
             .EqualTo('0')
             .Repeat(1)
-            .Select(fun zero -> zero.ToString())
+            .Select(fun zero -> zero.[0].ToString())
     
     let integerParser =
-        zeroIntegerParser.Or(notZeroIntegerParser)
+        notZeroIntegerParser.Try().Or(zeroIntegerParser)
--- OneTests.Parser/IntegerLiteral.Tests.fs
+++ OneTests.Parser/IntegerLiteral.Tests.fs
@@ -1,4 +1,4 @@
-module IntegerLiteral.Tests
+module public IntegerLiteral.Tests
 
 open System
 open FsUnit
@@ -25,6 +25,64 @@ let exactlyThreeDigits =
         yield [| "100" :> Object |]
     }
 
+let validFollowUpTokens =
+    seq {
+        yield [| "_000" :> Object |]
+        yield [| "_001" :> Object |]
+        yield [| "_010" :> Object |]
+        yield [| "_100" :> Object |]
+    }
+
+let invalidFollowUpTokens =
+    seq {
+        yield [| "000" :> Object |]
+        yield [| "001_" :> Object |]
+        yield [| "_010_" :> Object |]
+        yield [| "_a01" :> Object |]
+        yield [| "_0011" :> Object |]
+    }
+
+let validNotZeroIntegerLiterals =
+    seq {
+        yield [| "1" :> Object |]
+        yield [| "10" :> Object |]
+        yield [| "100" :> Object |]
+        yield [| "1_000" :> Object |]
+        yield [| "100_000" :> Object |]
+        yield [| "1_000_000" :> Object |]
+    }
+
+let invalidNotZeroIntegerLiterals =
+    seq {
+        yield [| "0" :> Object |]
+        yield [| "1000" :> Object |]
+        yield [| "_000" :> Object |]
+        yield [| "100_" :> Object |]
+        yield [| "01_000_000" :> Object |]
+    }
+
+let validIntegerLiterals =
+    seq {
+        yield [| "0" :> Object |]
+        yield [| "1" :> Object |]
+        yield [| "10" :> Object |]
+        yield [| "100" :> Object |]
+        yield [| "1_000" :> Object |]
+        yield [| "100_000" :> Object |]
+        yield [| "1_000_000" :> Object |]
+        yield [| "-1" :> Object |]
+        yield [| "-10" :> Object |]
+        yield [| "-100" :> Object |]
+        yield [| "-1_000" :> Object |]
+        yield [| "-100_000" :> Object |]
+        yield [| "-1_000_000" :> Object |]
+    }
+
+let allCharactersExceptZero =
+    seq {
+        for character in ['1' .. 'z'] do yield [| character.ToString() :> Object |]
+    }
+
 [<Theory>]
 [<MemberData("numberCharsWithoutZero")>]
 let ``notZeroDigitParser accepts digits w/o zero`` (item : char) =
@@ -43,17 +101,26 @@ let ``twoDigitParserWithoutLeadingZeroParser rejects two digits with leading zer
     twoDigitParserWithoutLeadingZeroParser.IsMatch(new TextSpan("01")) |> should be False
 
 [<Fact>]
-let ``threeDigitParserWithoutLeadingZeroParser accepts two digits w/o leading zero`` () =
+let ``threeDigitParserWithoutLeadingZeroParser accepts three digits w/o leading zero`` () =
     threeDigitParserWithoutLeadingZeroParser.IsMatch(new TextSpan("100")) |> should be True
 
 [<Fact>]
-let ``threeDigitParserWithoutLeadingZeroParser rejects two digits with leading zero`` () =
+let ``threeDigitParserWithoutLeadingZeroParser produces expected results`` () =
+    threeDigitParserWithoutLeadingZeroParser.Parse("100") |> should equal "100"
+
+[<Fact>]
+let ``threeDigitParserWithoutLeadingZeroParser rejects three digits with leading zero`` () =
     threeDigitParserWithoutLeadingZeroParser.IsMatch(new TextSpan("011")) |> should be False
 
 [<Theory>]
 [<MemberData("upToThreeDigitsWithoutLeadingZero")>]
 let ``leftMostThreeDigitGroupParser accepts up to three digits w/o leading zero`` (item : string) =
     leftMostThreeDigitGroupParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("upToThreeDigitsWithoutLeadingZero")>]
+let ``leftMostThreeDigitGroupParser produces expected results`` (item : string) =
+    leftMostThreeDigitGroupParser.Parse(item) |> should equal item
 
 [<Fact>]
 let ``leftMostThreeDigitGroupParser rejects tokens with more than 3 digits`` () =
@@ -69,5 +136,61 @@ let ``leftMostThreeDigitGroupParser rejects empty tokens`` () =
 
 [<Theory>]
 [<MemberData("exactlyThreeDigits")>]
-let ``threeDigitGroupParser`` accepts any three digits (item : string) =
+let ``threeDigitGroupParser accepts any three digits`` (item : string) =
     threeDigitGroupParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("exactlyThreeDigits")>]
+let ``threeDigitGroupParser produces expected results`` (item : string) =
+    threeDigitGroupParser.Parse(item) |> should equal item
+
+[<Theory>]
+[<MemberData("validFollowUpTokens")>]
+let ``threeDigitFollowUpParser accepts vald follow up tokens`` (item : string) =
+    threeDigitFollowUpParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("invalidFollowUpTokens")>]
+let ``threeDigitFollowUpParser rejects vald follow up tokens`` (item : string) =
+    threeDigitFollowUpParser.IsMatch(new TextSpan(item)) |> should be False
+
+[<Theory>]
+[<MemberData("validNotZeroIntegerLiterals")>]
+let ``notZeroIntegerParser accepts all vald integer literals`` (item : string) =
+    notZeroIntegerParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("validNotZeroIntegerLiterals")>]
+let ``notZeroIntegerParser produces expected result`` (item : string) =
+    notZeroIntegerParser.Parse(item) |> should equal (item.Replace("_", ""))
+
+[<Theory>]
+[<MemberData("invalidNotZeroIntegerLiterals")>]
+let ``notZeroIntegerParser rejects all invald integer literals`` (item : string) =
+    notZeroIntegerParser.IsMatch(new TextSpan(item)) |> should be False
+
+[<Fact>]
+let ``zeroIntegerParser accepts zero literal`` () =
+    zeroIntegerParser.IsMatch(new TextSpan("0")) |> should be True
+
+[<Fact>]
+let ``zeroIntegerParser produces expected result`` () =
+    zeroIntegerParser.Parse("0") |> should equal "0"
+
+[<Theory>]
+[<MemberData("allCharactersExceptZero")>]
+let ``zeroIntegerParser rejects all literals except zero`` (item : string) =
+    zeroIntegerParser.IsMatch(new TextSpan(item)) |> should be False
+
+[<Theory>]
+[<MemberData("validIntegerLiterals")>]
+let ``integerParser accepts all valid literals`` (item : string) =
+    integerParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("validIntegerLiterals")>]
+let ``integerParser produces expected results`` (item : string) =
+    integerParser.Parse (item) |> should equal (item.Replace("_", ""))
+
+let ``integerParser rejects minus zero`` () =
+    zeroIntegerParser.IsMatch(new TextSpan("-0")) |> should be False
