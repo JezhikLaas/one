673eb814920e749f02bce8a5d44a5fa46ee46bc4
Jezhik <uwe.laas@domla.de>
2019-05-19 16:58:17 +0200

    Adds identifier parser for identifiers starting with a upper case letter and containing underscores

--- One.Parser/Identifier.fs
+++ One.Parser/Identifier.fs
@@ -1,14 +1,26 @@
 module One.Parser.Identifier
 
+    open System
     open Superpower
     open Superpower.Parsers
 
     let lowerCaseIdentifierParser =
         Character.Lower
-            .Then(fun head -> Character.LetterOrDigit.Many())
-            .Select(fun tail -> new string(tail))
+            .Then(fun head -> Character.LetterOrDigit.Many()
+                                .Select(fun tail -> head.ToString() + new string(tail)))
     
     let upperCaseIdentifierParser =
         Character.Upper
-            .Then(fun head -> Character.LetterOrDigit.Many())
-            .Select(fun tail -> new string(tail))
+            .Then(fun head -> Character.LetterOrDigit.Many()
+                                .Select(fun tail -> head.ToString() + new string(tail)))    
+
+    let upperCaseIdentifierWithUnderscoreParser =
+        Character.Upper
+            .Then(fun head -> Character.Lower.Try().Or(Character.Digit).Select(fun character -> character.ToString())
+                                  .Try()
+                                  .Or(Character.EqualTo('_')
+                                          .Then(fun underscore -> Character.Upper.Try().Or(Character.Digit))
+                                          .Select(fun character -> "_" + (character.ToString()))
+                                  )
+                                  .Many()
+                                .Select(fun tail -> head.ToString() + String.Join(String.Empty, tail)))
--- OneTests.Parser/Identifier.Tests.fs
+++ OneTests.Parser/Identifier.Tests.fs
@@ -35,10 +35,30 @@ let invalidUpperCaseIdentifiers =
         yield [|"A_123" :> Object|]
     }
 
+let validUpperCaseIdentifierWithUnderscoreIdentifiers =
+    seq {
+        yield [|"Lower_Case_Identifier_Parser" :> Object|]
+        yield [|"Boolean_Literal_Parser" :> Object|]
+        yield [|"A_123" :> Object|]
+    }
+
+let invalidUpperCaseIdentifierWithUnderscoreIdentifiers =
+    seq {
+        yield [|"lowerC_aseI_dentifierP_arser" :> Object|]
+        yield [|"1Boolean_Literal_Parser" :> Object|]
+        yield [|"BooleanLiteralParser" :> Object|]
+        yield [|"AA_123" :> Object|]
+    }
+
 [<Theory>]
 [<MemberData("validLowerCaseIdentifiers")>]
 let ``lowerCaseIdentifierParser accepts identifiers starting with a lowercase letter``(item : string) =
     lowerCaseIdentifierParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("validLowerCaseIdentifiers")>]
+let ``lowerCaseIdentifierParser yields input as result``(item : string) =
+    lowerCaseIdentifierParser.Parse(item) |> should equal item
 
 [<Theory>]
 [<MemberData("invalidLowerCaseIdentifiers")>]
@@ -51,6 +71,26 @@ let ``upperCaseIdentifierParser accepts identifiers starting with a lowercase le
     upperCaseIdentifierParser.IsMatch(new TextSpan(item)) |> should be True
 
 [<Theory>]
+[<MemberData("validUpperCaseIdentifiers")>]
+let ``upperCaseIdentifierParser yields input as result``(item : string) =
+    upperCaseIdentifierParser.Parse(item) |> should equal item
+
+[<Theory>]
 [<MemberData("invalidUpperCaseIdentifiers")>]
 let ``upperCaseIdentifierParser rejects invalid identifiers``(item : string) =
     upperCaseIdentifierParser.IsMatch(new TextSpan(item)) |> should be False
+
+[<Theory>]
+[<MemberData("validUpperCaseIdentifierWithUnderscoreIdentifiers")>]
+let ``upperCaseIdentifierWithUnderscoreParser accepts identifiers starting with a lowercase letter``(item : string) =
+    upperCaseIdentifierWithUnderscoreParser.IsMatch(new TextSpan(item)) |> should be True
+
+[<Theory>]
+[<MemberData("validUpperCaseIdentifierWithUnderscoreIdentifiers")>]
+let ``upperCaseIdentifierWithUnderscoreParser yields input as result``(item : string) =
+    upperCaseIdentifierWithUnderscoreParser.Parse(item) |> should equal item
+
+[<Theory>]
+[<MemberData("invalidUpperCaseIdentifierWithUnderscoreIdentifiers")>]
+let ``upperCaseIdentifierWithUnderscoreParser rejects invalid identifiers``(item : string) =
+    upperCaseIdentifierWithUnderscoreParser.IsMatch(new TextSpan(item)) |> should be False
