924ac51168a19b73d569beac298e58c6794a320d
Jezhik <uwe.laas@domla.de>
2019-05-21 06:06:29 +0200

    Start tokenizer

--- One.Parser/OneToken.fs
+++ One.Parser/OneToken.fs
@@ -1,12 +1,45 @@
 namespace One.Parser
 
+    open Superpower.Display
+    
     type OneToken =
-        BooleanLiteral
-        | DecimalLiteral
-        | DoubleLiteral
-        | FloatLiteral
-        | IntegerLiteral
-        | LowerCaseIdentifier
-        | StringLiteral
-        | UpperCaseIdentifier
-        | UpperCaseWithUnderscoreIdentifier
+        | [<Token(Example = "and")>] And                                                      =   1
+        | [<Token(Example = ":=")>] Assign                                                    =   2
+        | [<Token(Example = "true | false")>] BooleanLiteral                                  =   3
+        | [<Token(Category = "keyword", Example = "class")>] Class                            =   4
+        | [<Token(Example = "check")>] Check                                                  =   5
+        | [<Token(Example = ":")>] Colon                                                      =   6
+        | [<Token(Example = ",")>] Comma                                                      =   7
+        | [<Token(Example = "create")>] Create                                                =   8
+        | [<Token(Example = "Current")>] Current                                              =   9
+        | DecimalLiteral                                                                      =  10
+        | [<Token(Example = "do")>] Do                                                        =  11
+        | DoubleLiteral                                                                       =  12
+        | [<Token(Example = "end")>] End                                                      =  13
+        | [<Token(Example = "ensure")>] Ensure                                                =  14
+        | [<Token(Example = "=")>] Equal                                                      =  15
+        | FloatLiteral                                                                        =  16
+        | [<Token(Example = "implies")>] Implies                                              =  17
+        | IntegerLiteral                                                                      =  18
+        | [<Token(Example = "inspect")>] Inspect                                              =  19
+        | [<Token(Example = "invariant")>] Invariant                                          =  20
+        | [<Token(Example = ">")>] Larger                                                     =  21
+        | [<Token(Example = ">=")>] LargerOrEqual                                             =  22
+        | [<Token(Example = "{")>] LeftBracket                                                =  23
+        | [<Token(Example = "(")>] LeftParenthesis                                            =  24
+        | [<Token(Example = "[")>] LeftSquareBracket                                          =  25
+        | [<Token(Example = "identifier")>] LowerCaseIdentifier                               =  26
+        | [<Token(Example = "not")>] Not                                                      =  27
+        | [<Token(Example = "<>")>] NotEqual                                                  =  28
+        | [<Token(Example = "or")>] Or                                                        =  29
+        | [<Token(Example = "require")>] Require                                              =  30
+        | [<Token(Example = "}")>] RightBracket                                               =  31
+        | [<Token(Example = ")")>] RightParenthesis                                           =  32
+        | [<Token(Example = "]")>] RightSquareBracket                                         =  33
+        | [<Token(Example = "<")>] Smaller                                                    =  34
+        | [<Token(Example = "<=")>] SmallerOrEqual                                            =  35
+        | StringLiteral                                                                       =  36
+        | [<Token(Example = "Identifier")>] UpperCaseIdentifier                               =  37
+        | [<Token(Example = "Identifier_With_Underscore")>] UpperCaseWithUnderscoreIdentifier =  38
+        | [<Token(Example = "when")>] When                                                    =  39
+        | [<Token(Example = "xor")>] Xor                                                      =  40
--- One.Parser/OneTokenizer.fs
+++ One.Parser/OneTokenizer.fs
@@ -13,6 +13,23 @@ module One.Parser.OneTokenizer
     let tokenizer =
         builder
             .Ignore(Span.WhiteSpace)
+            // keywords
+            .Match(Span.EqualTo("and"), OneToken.And)
+            .Match(Span.EqualTo("class"), OneToken.Class)
+            .Match(Span.EqualTo("check"), OneToken.Check)
+            .Match(Span.EqualTo("create"), OneToken.Create)
+            .Match(Span.EqualTo("Current"), OneToken.Current)
+            .Match(Span.EqualTo("do"), OneToken.Do)
+            .Match(Span.EqualTo("end"), OneToken.End)
+            .Match(Span.EqualTo("ensure"), OneToken.Ensure)
+            .Match(Span.EqualTo("inspect"), OneToken.Inspect)
+            .Match(Span.EqualTo("invariant"), OneToken.Invariant)
+            .Match(Span.EqualTo("not"), OneToken.Not)
+            .Match(Span.EqualTo("or"), OneToken.Or)
+            .Match(Span.EqualTo("require"), OneToken.Require)
+            .Match(Span.EqualTo("when"), OneToken.When)
+            .Match(Span.EqualTo("xor"), OneToken.Xor)
+            // recognizers
             .Match(booleanLiteralRecognizer, OneToken.BooleanLiteral)
             .Match(decimalLiteralRecognizer, OneToken.DecimalLiteral)
             .Match(doubleLiteralRecognizer, OneToken.DoubleLiteral)
